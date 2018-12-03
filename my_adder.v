b761738dbc7705f9e751b95a1fd22ea09c151b87
MerelyLogical <qieerbushe7@gmail.com>
2018-12-03 14:57:52 +0000

    Avalon slave for adder

--- my_adder.v
+++ my_adder.v
@@ -1,11 +1,42 @@
 module my_adder #(
-	parameter WIDTH = 8
+	parameter WIDTH = 32
 )(
-	input  wire [7:0] a,
-	input  wire [7:0] b,
-	output wire [7:0] o
+	input clk,
+	input reset,
+	
+	// Avalon slave
+	input            [3:0] slave_address,
+	input                  slave_read,
+	input                  slave_write,
+	input      [WIDTH-1:0] slave_writedata,
+	output reg [WIDTH-1:0] slave_readdata
 );
 
+	// internal vars
+	reg  [WIDTH-1:0] a;
+	reg  [WIDTH-1:0] b;
+	wire [WIDTH-1:0] o;
+	
+	// Avalon slave logic
+	localparam A_ADDR = 4'h0;
+	localparam B_ADDR = 4'h4;
+	localparam O_ADDR = 4'h8;
+	
+	always @(posedge clk) begin
+		case (slave_address)
+			A_ADDR:
+				if (~slave_read && slave_write)
+					a <= slave_writedata;
+			B_ADDR:
+				if (~slave_read && slave_write)
+					b <= slave_writedata;
+			O_ADDR:
+				if (slave_read && ~slave_write)
+					slave_readdata <= o;
+		endcase
+	end
+
+	// adder logic
 	assign o = a + b;
 
 endmodule
